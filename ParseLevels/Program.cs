e7f76a5d35456da44715ab71f7b0c528ea2758d3
Rob Prouse <rob@prouse.org>
2019-05-13 07:57:45 -0400

    Working on parsing txt puzzles and generating the code for the levels

--- ParseLevels/Program.cs
+++ ParseLevels/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
 
 namespace ParseLevels
 {
@@ -6,7 +9,62 @@ namespace ParseLevels
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if(args.Length != 1 || !File.Exists(args[0]))
+            {
+                Console.WriteLine("Usage: ParseLevels <filename>");
+                return;
+            }
+
+            var pack = Path.GetFileNameWithoutExtension(args[0]);
+            string name = null;
+            List<string> puzzle = new List<string>();
+
+            using(var file = new System.IO.StreamReader(args[0]))
+            {
+                string line = file.ReadLine();
+                while(line != null)
+                {
+                    if(line.StartsWith(";"))
+                    {
+                        name = line.Substring(2);
+                        puzzle.Clear();
+                    }
+                    else if(string.IsNullOrWhiteSpace(line))
+                    {
+                        if(puzzle.Count > 0)
+                        {
+                            ParsePuzzle(pack, name, puzzle);
+                        }
+                    }
+                    else
+                    {
+                        puzzle.Add(line);
+                    }
+                    line = file.ReadLine();
+                }
+            }
         }
+
+        static void ParsePuzzle(string pack, string name, List<string> puzzle)
+        {
+            int width = longestLine(puzzle);
+            int height = puzzle.Count;
+
+            if(width > 16 || height > 16)
+                return;
+
+            if(width > 8 && height > 8)
+                return;
+
+            // Switch to landscape mode for the arduboy wide screen
+            bool rotate = height > width;
+
+            char[,] level = new char[8,16];
+
+            Console.WriteLine($"// {pack} level {name}");
+        }
+
+        static int longestLine(IEnumerable<string> puzzle) =>
+            puzzle.Max(s => s.Length);
     }
 }
